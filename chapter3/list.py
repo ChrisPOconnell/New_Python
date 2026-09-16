689a949cef737d9e8b37ca82c3308504a12eb4e1
Chris P. OConnell <ChrisPOconnell@users.noreply.github.com>
2026-09-16 07:26:40 -0700

    syncin' stuff back up

--- chapter3/list.py
+++ chapter3/list.py
@@ -1,5 +1,23 @@
 #This exercise creates a list
-coworkers = ['Alice', 'Ashmeeta', 'Bob', 'Charlie', 'David', 'Eve']
+coworkers = ['Waseel', 'Alice', 'Ashmeeta', 'Bob', 'Charlie', 'David', 'Eve']
 
 message = f"hello {coworkers[0].title()}!"
 print(message)
+
+coworkers.insert(0, "Frank")
+
+#removing a list item by value.
+coworkers.remove("Bob")
+
+print(coworkers)
+
+new_coworkers = ["Alice", "Bob"]
+print(new_coworkers)
+
+#sorting
+coworkers_alpha = coworkers.sort()
+coworkers_reverse = coworkers.sort(reverse=True)
+print(f"Sorted coworkers: {coworkers_alpha}")
+print(f"Reverse alpha {coworkers_reverse}")
+
+print(coworkers)
